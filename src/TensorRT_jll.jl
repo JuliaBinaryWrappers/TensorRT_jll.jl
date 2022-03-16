d12892cccb6e3a658c1566c9f731d748eed9e40a
jlbuild <juliabuildbot@gmail.com>
2022-03-16 07:47:59 +0000

    TensorRT_jll build 8.0.1+0

---
+++ src/TensorRT_jll.jl
@@ -0,0 +1,10 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule TensorRT_jll
+using Base
+using Base: UUID
+using LazyArtifacts
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("TensorRT")
+JLLWrappers.@generate_main_file("TensorRT", UUID("2eaff018-c6f9-5ddb-97b6-8b0d18888fbb"))
+end  # module TensorRT_jll
